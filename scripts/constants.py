05124aa27dfd91b9e31bf0d7904790b51ddbb23d
Nikol Chantzi <nicolechantzi@gmail.com>
2025-05-13 20:22:22 -0400

    added data and moved some data to different folders

--- scripts/constants.py
+++ scripts/constants.py
@@ -10,47 +10,47 @@ load_dotenv()
 
 class ConfigPaths(Enum):
 
-    CENTROMERE = Path(os.getenv("CENTROMERE"))
-    TELOMERE = Path(os.getenv("TELOMERE"))
+    CENTROMERE = os.getenv("CENTROMERE")
+    TELOMERE = os.getenv("TELOMERE")
     # SEQUENCE_SIZES = Path(os.getenv("SEQUENCE_SIZES"))
-    GENOME_SIZE = Path(os.getenv("GENOME_SIZE"))
-    INDEX = Path(os.getenv("INDEX"))
-    FASTA = Path(os.getenv("FASTA"))
+    GENOME_SIZE = os.getenv("GENOME_SIZE")
+    INDEX = os.getenv("INDEX")
+    FASTA = os.getenv("FASTA")
 
     # TRIMERS = Path(os.getenv("TRIMERS"))
-    G4HUNTER = Path(os.getenv("G4HUNTER"))
-    CONTROL_G4HUNTER = Path(os.getenv("CONTROL_G4HUNTER"))
+    G4HUNTER = os.getenv("G4HUNTER")
+    CONTROL_G4HUNTER = os.getenv("CONTROL_G4HUNTER")
 
     # METHYLATION DATASETS
-    HG002_METH = Path(os.getenv("HG002_METH"))
-    CHM13v2_METH = Path(os.getenv("CHM13v2_METH"))
+    HG002_METH = os.getenv("HG002_METH")
+    CHM13v2_METH = os.getenv("CHM13v2_METH")
 
     # METADATA
-    METADATA = Path(os.getenv("METADATA"))
+    METADATA = os.getenv("METADATA")
 
     # G4 DATASETS
-    G4HUNTER_HG002 = Path(os.getenv("G4HUNTER_HG002"))
-    G4REGEX = Path(os.getenv("G4REGEX"))
-    CONTROL_G4REGEX = Path(os.getenv("CONTROL_G4REGEX"))
+    G4HUNTER_HG002 = os.getenv("G4HUNTER_HG002")
+    G4REGEX = os.getenv("G4REGEX")
+    CONTROL_G4REGEX = os.getenv("CONTROL_G4REGEX")
 
     # SEQUENCE REPORT
-    SEQUENCE_REPORT = Path(os.getenv("SEQUENCE_REPORT"))
+    SEQUENCE_REPORT = os.getenv("SEQUENCE_REPORT")
 
     # GFF
-    GFF = Path(os.getenv("GFF"))
-    GFF_AGAT = Path(os.getenv("GFF_AGAT"))
+    GFF = os.getenv("GFF")
+    GFF_AGAT = os.getenv("GFF_AGAT")
 
     # MISCALLENEOUS
     # PROMOTERS = Path(os.getenv("PROMOTERS"))
-    GERM_VAR = Path(os.getenv("GERM_VAR"))
-    MUTATION = Path(os.getenv("MUTATION"))
+    GERM_VAR = os.getenv("GERM_VAR")
+    MUTATION = os.getenv("MUTATION")
 
     # directory of processed/splitted mutations from Germline Variants
-    PROCESSED_MUTATIONS = Path(os.getenv("PROCESSED_MUTATIONS"))
-    PRMD9 = Path(os.getenv("PRMD9"))
+    PROCESSED_MUTATIONS = os.getenv("PROCESSED_MUTATIONS")
+    PRMD9 = os.getenv("PRMD9")
 
     def __init__(self, path):
-        if not path.exists():
+        if path and not Path(path).resolve().exists():
             raise FileNotFoundError(f"Path does not exist: {path}.")
 
 if __name__ == "__main__":
